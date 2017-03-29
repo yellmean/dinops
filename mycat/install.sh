bd52d857e6a4ce9f296eeb9ccffde2d1eb40a396
yellmean <yellmean@users.noreply.github.com>
2017-03-29 08:47:59 +0800

    Update install.sh

--- mycat/install.sh
+++ mycat/install.sh
@@ -10,12 +10,12 @@ MysqlReadUrl=192.168.0.1:3306
 MysqlWriteUrl=192.168.0.2:3306
 MysqlUser=dingjian
 MysqlPassword=dingjian
-DownloadLink=https://github.com/MyCATApache/Mycat-download/raw/master/1.5-RELEASE/Mycat-server-1.5.1-RELEASE-20161110104159-linux.tar.gz
+DownloadLink='http://dl.mycat.io/1.6-RELEASE/Mycat-server-1.6-RELEASE-20161028204710-linux.tar.gz'
 
 function checksource {
     cd $ProgDir || return $?
-    if [[ -n $(ls | grep -w 'Mycat-server-1.5.1-RELEASE-20161110104159-linux.tar.gz') ]]; then
-         echo "Mycat-server-1.5.1-RELEASE-20161110104159-linux.tar.gz already exists!"
+    if [[ -n $(ls | grep -w 'Mycat-server-1.6-RELEASE-20161028204710-linux.tar.gz') ]]; then
+         echo "Mycat-server-1.6-RELEASE-20161028204710-linux.tar.gz already exists!"
     else
          wget $DownloadLink || return $?
     fi
@@ -35,10 +35,10 @@ function environment {
     return 0
 }
 
-function install {
+function installing {
     cd $ProgDir || return $?
     echo "Start installing Mycat Server..."
-    tar zxvf Mycat-server-1.5.1-RELEASE-20161110104159-linux.tar.gz -C $MycatHome || return $?
+    tar zxvf Mycat-server-1.6-RELEASE-20161028204710-linux.tar.gz -C $MycatHome || return $?
     echo "Install Mycat Server success! It home directory is $MycatHome/mycat."
     return 0;
 }
@@ -50,22 +50,23 @@ function configure {
     sed -i "s/TESTDB/$MycatSchema/g" conf/server.xml
     sed -i "s/TESTDB/$MycatSchema/g" conf/schema.xml
     #cat << EOF > conf/schema.xml
-    echo '<?xml version="1.0"?>' > conf/schema.xml
-    echo '<!DOCTYPE mycat:schema SYSTEM "schema.dtd">' >> conf/schema.xml
-    echo '<mycat:schema xmlns:mycat="http://org.opencloudb/" >' >> conf/schema.xml
-    echo "\\t<schema name=\"$MycatSchema\" checkSQLschema=\"false\" sqlMaxLimit=\"100\" dataNode=\"dn1\"></schema>" >> conf/schema.xml
-    echo "\\t<dataNode name=\"dn1\" dataHost=\"localhost1\" database=\"$MycatSchema\" />" >> conf/schema.xml
-    echo "\\t<dataHost name=\"localhost1\" maxCon=\"1000\" minCon=\"10\" balance=\"0\" writeType=\"0\" dbType=\"mysql\" dbDriver=\"native\" switchType=\"1\"  slaveThreshold=\"100\">" >> conf/schema.xml
-    echo "\\t\\t<heartbeat>select user()</heartbeat>" >> conf/schema.xml
-    echo "\\t\\t<writeHost host=\"hostM1\" url=\"$MysqlWriteUrl\" user=\"$MysqlUser\" password=\"$MysqlPassword\">" >> conf/schema.xml
-    echo "\\t\\t<readHost host=\"hostS1\" url=\"$MysqlReadUrl\" user=\"$MysqlUser\" password=\"$MysqlPassword\" />" >> conf/schema.xml
-    echo "\\t\\t\\t</writeHost>" >> conf/schema.xml
-    echo "\\t</dataHost>" >> conf/schema.xml
-    echo '</mycat:schema>' >> conf/schema.xml
-    echo "Configure Mycat success!"
+    echo -e '<?xml version="1.0"?>' > conf/schema.xml
+    echo -e '<!DOCTYPE mycat:schema SYSTEM "schema.dtd">' >> conf/schema.xml
+    echo -e '<mycat:schema xmlns:mycat="http://org.opencloudb/" >' >> conf/schema.xml
+    echo -e "\t<schema name=\"$MycatSchema\" checkSQLschema=\"false\" sqlMaxLimit=\"100\" dataNode=\"dn1\"></schema>" >> conf/schema.xml
+    echo -e "\t<dataNode name=\"dn1\" dataHost=\"localhost1\" database=\"$MycatSchema\" />" >> conf/schema.xml
+    echo -e "\t<dataHost name=\"localhost1\" maxCon=\"1000\" minCon=\"10\" balance=\"0\" writeType=\"0\" dbType=\"mysql\" dbDriver=\"native\" switchType=\"1\"  slaveThreshold=\"100\">" >> conf/schema.xml
+    echo -e "\t\t<heartbeat>select user()</heartbeat>" >> conf/schema.xml
+    echo -e "\t\t<writeHost host=\"hostM1\" url=\"$MysqlWriteUrl\" user=\"$MysqlUser\" password=\"$MysqlPassword\">" >> conf/schema.xml
+    echo -e "\t\t\t<readHost host=\"hostS1\" url=\"$MysqlReadUrl\" user=\"$MysqlUser\" password=\"$MysqlPassword\" />" >> conf/schema.xml
+    echo -e "\t\t</writeHost>" >> conf/schema.xml
+    echo -e "\t</dataHost>" >> conf/schema.xml
+    echo -e '</mycat:schema>' >> conf/schema.xml
+    echo -e "Configure Mycat success!"
     return 0
 }
 
 checksource; [ $? -ne 0 ] && exit 1
 environment; [ $? -ne 0 ] && exit 2
-configure $1; [ $? -ne 0 ] && exit 3
+installing; [ $? -ne 0 ] && exit 3
+configure $1; [ $? -ne 0 ] && exit 4
